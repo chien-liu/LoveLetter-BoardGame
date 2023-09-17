49ec39b8a3e788c400e6f3e7863e9b82b0f8e3e3
chien-liu <liu_chien@gapp.nthu.edu.tw>
2023-09-17 23:47:36 +0200

    Rewrite card class make its better interface better

--- src/card.cpp
+++ src/card.cpp
@@ -2,51 +2,37 @@
 
 #include <assert.h>
 
-#include <algorithm>  // std::random_shuffle
+#include <algorithm> // std::random_shuffle
 #include <iostream>
 
-namespace loveletter{
+namespace loveletter
+{
+  Card::Card(int n) : num(n), name(initializeName(n)){};
+  Card::Card(const Card &card) : num(card.getNum()), name(card.getName()){};
 
-bool Card::operator==(const Card& other) const { return num == other.getNum(); }
-bool Card::operator!=(const Card& other) const { return !operator==(other); }
-bool Card::operator>(const Card& other) const { return num > other.getNum(); }
-bool Card::operator<(const Card& other) const { return num < other.getNum(); }
-
-
-std::ostream& operator<<(std::ostream& stream, const Card& card) {
-  stream << card.name << "(" << card.num << ")";
-  return stream;
-}
-
-const std::map<int, const char*> Card::dict = {
-    {0, ""},      {1, "Guard"},    {2, "Priest"},
-    {3, "Baron"}, {4, "Handmaid"}, {5, "Prince"},
-    {6, "King"},  {7, "Countess"}, {8, "Princess"}};
-
-CardPool::CardPool(int n_player) {
-  pile.reserve(16);
-  for (int i : {1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8}) {
-    pile.push_back(std::move(Card(i)));
+  const std::string &Card::initializeName(int n)
+  {
+    if (n < 1 || n > 8)
+      throw std::invalid_argument("Card::Card(int n), n needs to be in the range of [1, 8].\n");
+    return dict.at(n);
   }
-  std::random_shuffle(pile.begin(), pile.end());
 
-  reserved_card = next();
-  if (n_player == 2) {
-    pile.erase(pile.begin(), pile.begin() + 2);
+  int Card::getNum() const noexcept { return num; }
+  const std::string &Card::getName() const noexcept { return name; }
+
+  bool Card::operator==(const Card &other) const { return num == other.getNum(); }
+  bool Card::operator!=(const Card &other) const { return !operator==(other); }
+  bool Card::operator>(const Card &other) const { return num > other.getNum(); }
+  bool Card::operator<(const Card &other) const { return num < other.getNum(); }
+  bool Card::operator>=(const Card &other) const { return num >= other.getNum(); }
+  bool Card::operator<=(const Card &other) const { return num <= other.getNum(); }
+
+  std::ostream &operator<<(std::ostream &stream, const Card &card)
+  {
+    stream << card.name << "(" << card.num << ")";
+    return stream;
   }
-}
 
-Card CardPool::next() {
-  assert(pile.size() > 0 && "No more cards");
-  Card card = pile.back();
-  pile.pop_back();
-  return card;
-}
-
-Card CardPool::GetReservedCard() {
-  assert(pile.size() == 0 && "Invalid Action: Special Draw");
-  Card card = reserved_card;
-  reserved_card = Card(0);
-  return card;
-}
+  const std::map<int, const std::string> Card::dict = {
+      {0, ""}, {1, "Guard"}, {2, "Priest"}, {3, "Baron"}, {4, "Handmaid"}, {5, "Prince"}, {6, "King"}, {7, "Countess"}, {8, "Princess"}};
 }
